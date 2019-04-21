a2694be7139b447f2d9ada09aecbf42771bb8018
Jinsung Sunwoo <jinsung.sunwoo@gmail.com>
2019-04-22 00:01:40 +0900

    잘되네

--- App.js
+++ App.js
@@ -8,7 +8,7 @@
 
 import React, { Component } from "react";
 import {
-  Platform,
+  // Platform,
   StyleSheet,
   Text,
   View,
@@ -16,41 +16,55 @@ import {
 } from "react-native";
 import Voice from "react-native-voice";
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+// const instructions = Platform.select({
+//   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+//   android:
+//     "Double tap R on your keyboard to reload,\n" +
+//     "Shake or press menu button for dev menu"
+// });
 
-type Props = {};
-export default class App extends Component<Props> {
-  check() {
-    Voice.start("en_US");
+// type Props = {};
+// export default class App extends Component<Props> {
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      results: []
+    };
+    Voice.onSpeechResults = this.onSpeechResults.bind(this);
+  }
+
+  onSpeechResults(e) {
+    this.setState({
+      results: e.value
+    });
+  }
+
+  start() {
+    Voice.start("ko_KR");
+    // Voice.start("en_US");
+    console.log("start");
+  }
+
+  stop() {
+    Voice.stop();
     console.log("start");
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity onPress={this.check.bind()}>
-          <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <TouchableOpacity onPress={this.start.bind()}>
+          <Text style={styles.instructions}>START</Text>
         </TouchableOpacity>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <TouchableOpacity onPress={this.stop.bind()}>
+          <Text style={styles.instructions}>STOP</Text>
+        </TouchableOpacity>
+        {this.state.results.map((result, index) => {
+          return <Text key={index}>{result}</Text>;
+        })}
       </View>
     );
-  }
-}
-
-class VoiceTest extends Component {
-  constructor(props) {
-    Voice.onSpeechStart = this.onSpeechStartHandler.bind(this);
-    Voice.onSpeechEnd = this.onSpeechEndHandler.bind(this);
-    Voice.onSpeechResults = this.onSpeechResultsHandler.bind(this);
-  }
-  onStartButtonPress(e) {
-    Voice.start("en-US");
   }
 }
 
